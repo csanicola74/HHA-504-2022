24bf62a40f43000a9928f882ce49fb619ea655ea
csanicola74 <caroline.sanicola@stonybrook.edu>
2022-09-24 17:07:59 -0400

    update

--- .history/Part4_DB_Sqlite/p1_createDb_20220911105104.py
+++ .history/Part4_DB_Sqlite/p1_createDb_20220911105104.py
@@ -1,16 +1,16 @@
-import sqlite3  # note, sqlite3 comes with python3
+import sqlite3 # note, sqlite3 comes with python3
 
-# Note, this is just an example script, to create a database, you need to do this manually.
-# otherwise you just need to run this once and then you can use the database in your flask app
+##### Note, this is just an example script, to create a database, you need to do this manually.
+## otherwise you just need to run this once and then you can use the database in your flask app
 
-# For a brief tutorial on sqlite using pythong, please visit: https://www.tutorialspoint.com/sqlite/sqlite_python.htm
-# You can scroll down to where it says 'Connect To Database'
-# For an additional tutorial that goes through the basics, please see: https://www.sqlitetutorial.net/sqlite-python/
+### For a brief tutorial on sqlite using pythong, please visit: https://www.tutorialspoint.com/sqlite/sqlite_python.htm 
+### You can scroll down to where it says 'Connect To Database' 
+### For an additional tutorial that goes through the basics, please see: https://www.sqlitetutorial.net/sqlite-python/ 
 
 # Connecting to sqlite
 # connection object
 connect = sqlite3.connect('./Part4_DB_Sqlite/patients.db')
-
+ 
 # db object
 db = connect.cursor()
 
@@ -18,10 +18,10 @@ db = connect.cursor()
 db.execute("DROP TABLE IF EXISTS patient_table")
 connect.commit()
 
-# // commit () --> This method commits the current transaction. If you don't call this method,
+# // commit () --> This method commits the current transaction. If you don't call this method, 
 # anything you did since the last call to commit() is not visible from other database connections.
 
-# Creating table,
+# Creating table, 
 table = """ CREATE TABLE patient_table (
             mrn VARCHAR(255) NOT NULL,
             firstname CHAR(25) NOT NULL,
@@ -30,11 +30,11 @@ table = """ CREATE TABLE patient_table (
         ); """
 
 db.execute(table)
-connect.commit()  # commit the changes, this is annoying but necessary
+connect.commit() # commit the changes, this is annoying but necessary
 
 
-# note, you may see a .db-journal file, that is a temporary file that is created when you create a database.
-# insert data into the table
+## note, you may see a .db-journal file, that is a temporary file that is created when you create a database.
+## insert data into the table
 db.execute("INSERT INTO patient_table(mrn, firstname, lastname, dob) values('12345', 'John', 'Smith', '01/01/2000')")
 db.execute("INSERT INTO patient_table(mrn, firstname, lastname, dob) values('23456', 'Jane', 'Doe', '02/02/2001')")
 db.execute("INSERT INTO patient_table(mrn, firstname, lastname, dob) values('34567', 'Mary', 'Smith', '03/03/2002')")
